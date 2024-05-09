3fd963cced10bad82262037cf9fe4956d72dc590
Nicolas Nouhi <nnouhi@uclan.ac.uk>
2024-05-09 22:24:17 +0300

    Installed entity framework work package and created the sql server connection

---
+++ ECommerseTemplate/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ECommerseTemplate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerseTemplate.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+ 
+        }
+
+        // Db tables
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
---
+++ ECommerseTemplate/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerseTemplate.Models
+{
+    public class CategoryModel
+    {
+        // This is called Data Annotation, basically we are saying this property will be the PK
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
