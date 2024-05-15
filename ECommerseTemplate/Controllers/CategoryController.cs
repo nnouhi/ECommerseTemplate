0e1e20d7eacded7cfa8444af9d84c32d7995f69a
Nicolas Nouhi <nnouhi@uclan.ac.uk>
2024-05-15 22:37:13 +0300

    Finalized CRUD operations for categories, also integrated toastr for visual feedback on a successful action

--- ECommerseTemplate/Controllers/CategoryController.cs
+++ ECommerseTemplate/Controllers/CategoryController.cs
@@ -30,6 +30,7 @@ namespace ECommerseTemplate.Controllers
             {
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 			    return RedirectToAction("Index");
 			}
 
@@ -56,7 +57,44 @@ namespace ECommerseTemplate.Controllers
 		[HttpPost]
 		public IActionResult Edit(CategoryModel obj)
 		{
-			// TODO
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
+				return RedirectToAction("Index");
+			}
+
+			return View();
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			CategoryModel fetchedCategory = _db.Categories.Find(id);
+			if (fetchedCategory == null)
+			{
+				return NotFound();
+			}
+
+			return View(fetchedCategory);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(CategoryModel obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Remove(obj);
+				_db.SaveChanges();
+				TempData["success"] = "Category deleted successfully";
+				return RedirectToAction("Index");
+			}
+
 			return View();
 		}
 	}
