5285456995e744f7760b9ffc289aa17344aa92a9
Nicolas Nouhi <nnouhi@uclan.ac.uk>
2024-07-17 14:07:16 +0300

    Implemented admin panel to verify/hide certain reviews

---
+++ ECommerseTemplate/Areas/Admin/Controllers/ProductReviewController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerseTemplate.DataAccess.Repository.IRepository;
+using ECommerseTemplate.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace ECommerseTemplate.Areas.Admin.Controllers
+{
+	[Area(SD.Roles.Admin)]
+	[Authorize(Roles = SD.Roles.Admin)]
+	public class ProductReviewController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ProductReviewController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			List<ProductReview> productReviews = _unitOfWork.ProductReview.GetAll(includeProperties: "Product").ToList();
+			return View(productReviews);
+		}
+
+		#region API CALLS
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<ProductReview> productReviews = _unitOfWork.ProductReview.GetAll(includeProperties: "Product").ToList();
+			// Process each review
+			foreach (ProductReview review in productReviews)
+			{
+				review.Images = _unitOfWork.ProductReviewImage.GetAll(pri => pri.ProductReviewId == review.Id)
+															   .Select(pri => pri.Path)
+															   .ToList();
+			}
+			return Json(new { data = productReviews });
+		}
+
+		[HttpGet]
+		public IActionResult VerifyReview(int id, bool verify)
+		{
+			ProductReview productReview = _unitOfWork.ProductReview.Get(r => r.Id == id);
+			productReview.IsAdminApproved = verify;
+			_unitOfWork.ProductReview.Update(productReview);
+			_unitOfWork.Save();
+			return RedirectToAction(nameof(Index));
+		}
+		#endregion
+	}
+}
---
+++ ECommerseTemplate/wwwroot/js/productreview.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    $('#tblData').DataTable({
+        ajax: {
+            url: "/admin/productreview/getall",
+            type: "GET",
+            datatype: "json",
+        },
+        columns: [
+            { data: 'name', width: '20%' },
+            { data: 'email', width: '20%' },
+            { data: 'title', width: '20%' },
+            {
+                data: 'review',
+                width: '20%',
+                render: function (data) {
+                    // Truncate the review text and use ellipsis if it's too long
+                    let truncatedReview = data.length > 50 ? data.substring(0, 50) + '...' : data;
+                    return `<span title="${data}">${truncatedReview}</span>`;
+                }
+            },
+            { data: 'product.title', width: '20%' },
+            {
+                data: 'images',
+                width: '20%',
+                render: function (images) {
+                    const baseUrl = "https://localhost:7080/"; // Will need to be adjusted
+
+                    // Create clickable links for images, adjusting URLs to remove "/Admin"
+                    let imgLinks = images.map(imgPath => {
+                        // Remove "/Admin" from the path
+                        let adjustedPath = imgPath.replace('/Admin', '');
+
+                        return `<a href="${baseUrl}${adjustedPath}" target="_blank">Image
+                                </a>`;
+                    }).join('');
+
+                    return imgLinks;
+                }
+            },
+            {
+                data: { id: 'id', isAdminApproved: 'IsAdminApproved', width: '33%' },
+                render: function (data) {
+                    if (data.isAdminApproved) {
+                        return `
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/admin/productreview/verifyreview?id=${data.id}&verify=false" class="btn btn-danger mx-2"><i class="bi bi-eye-slash-fill"></i> Hide</a>
+                            </div>
+                        `;
+                    } else {
+                        return `
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/admin/productreview/verifyreview?id=${data.id}&verify=true" class="btn btn-primary mx-2"><i class="bi bi-eye-fill"></i> Verify</a>
+                            </div>
+                        `;
+                    }
+                },
+                width: "15%"
+            }
+        ],
+        destroy: true // Allows re-initialization of the DataTable
+    });
+}
